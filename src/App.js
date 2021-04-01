4e8a35a416f9d67e3a541e84d1ff8f5e80b96308
Diego Lopes <diegolopes087@gmail.com>
2021-03-31 21:49:48 -0300

    page basic layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
 
 function App() {
-  return <div className="main"></div>;
+  return (
+    <div className="main">
+      <div className="center-column">
+        <div className="title-box">
+          <h1>Simple, traffic-based pricing</h1>
+          <span>Sign-up for our 30-day trial. No credit card required</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
