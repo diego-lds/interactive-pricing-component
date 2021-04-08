97073ae41c46dc508c5edda61166624e832fedb0
Diego Lopes <diegolopes087@gmail.com>
2021-04-07 21:17:21 -0300

    Add labels from the top

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
-
+import { Box } from './components/box';
 function App() {
   return (
     <div className="main">
       <div className="center-column">
-        <div className="title-box">
+        <div className="box-title">
           <h1>Simple, traffic-based pricing</h1>
           <span>Sign-up for our 30-day trial. No credit card required</span>
         </div>
+        <Box>
+          <div className="box-top">
+            <span className="views">100K PAGEVIEWS</span>
+            <div>
+              <span className="amount">$16.00</span>
+              <span className="time">/month</span>
+            </div>
+          </div>
+        </Box>
       </div>
     </div>
   );
